584b1a94a3132b81c9afda5145f461d086e3f52b
Guilherme Keiel <gkeiel@hotmail.com>
2025-09-23 14:09:37 -0300

    v1.0.0

--- b3_trading_signals.py
+++ b3_trading_signals.py
@@ -7,10 +7,13 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    # define start and end time
-    start   = "2024-01-01"
-    end     = datetime.now()
-    results = []
+    # defines start and end time
+    start = "2024-01-01"
+    end   = datetime.now()
+
+    # initialize dictionaries
+    data_cache = {}
+    results    = {} 
 
     # import lists of parameters:
     # - tickers
@@ -18,29 +21,41 @@ def main():
     tickers    = tsf.load_tickers("tickers_test.txt")
     indicators = tsf.load_indicators("indicators_test.txt")
 
-    # run backtest for each ticker and strategy
+    # download data and run backtest (for each ticker and strategy)
     for ticker, (ma_s, ma_l) in itertools.product(tickers, indicators):
-        df = tsf.download_data(ticker, start, end)
+
+        # download data (once only)
+        if ticker not in data_cache:
+            data_cache[ticker] = tsf.download_data(ticker, start, end)
+        df = data_cache[ticker]
+
+        # run backtest
         df = tsf.run_strategy(df[["Close"]], ma_s, ma_l)
+
         market   = df["Cumulative_Market"].iloc[-1]
         strategy = df["Cumulative_Strategy"].iloc[-1]
 
         # export current dataframe for analysis
         df.to_excel("debug/df_debug.xlsx", index=True)
+
+        # initialize ticker list
+        if ticker not in results:
+            results[ticker] = []
         
         # store results
-        results.append({
-            "Ticker": ticker,
+        results[ticker].append({
             "MA_S": ma_s,
             "MA_L": ma_l,
-            "Final_Market": market,
-            "Final_Strategy": strategy
+            "Return_Market": market,
+            "Return_Strategy": strategy
         })
         tsf.plot_res(df, ticker, ma_s, ma_l)
 
-    # export results
-    results_df = pd.DataFrame(results)
-    results_df.to_excel("results/results_backtest.xlsx", index=False)
+    # export results (a spreadsheet for each ticker)
+    with pd.ExcelWriter("results/results_backtest.xlsx", engine="openpyxl") as writer:
+        for ticker, ticker_results in results.items():
+            ticker_results_df = pd.DataFrame(ticker_results)
+            ticker_results_df.to_excel(writer, sheet_name=ticker[:10], index=False)
 
 
 if __name__ == "__main__":
