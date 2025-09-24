fa96793c86d5e9d4a5ae366ea53d320536a4c801
Guilherme Keiel <gkeiel@hotmail.com>
2025-09-23 22:15:34 -0300

    v1.0.0

--- b3_trading_signals_bot.py
+++ b3_trading_signals_bot.py
@@ -5,11 +5,12 @@ from datetime import datetime
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-# insert best strategy
-ma_s = 5
-ma_l = 30
-indicators = []
-pair = (ma_s, ma_l)
+# import selected strategies:
+# - tickers
+# - indicators
+strategies = pd.read_csv("strategies.csv").set_index("Ticker").to_dict("index")
+tickers    = list(strategies.keys())
+
 
 def main():
     # define start and end time
@@ -17,19 +18,19 @@ def main():
     end    = datetime.now().strftime("%Y-%m-%d")
     alerts = []
 
-    # import parameters:
-    # - tickers
-    tickers = tsf.load_tickers("tickers_test.txt") 
-
     # run for each ticker
     for ticker in tickers:
-        print("Processing")
+        print(f"Processing {ticker}")
+        
+        # strategy
+        ma_s = strategies[ticker]["MA_S"]
+        ma_l = strategies[ticker]["MA_L"]
 
         # download and backtest
         df = tsf.download_data(ticker, start, end)
-        df = tsf.run_strategy(df[["Close"]], pair[0], pair[1])
+        df = tsf.run_strategy(df[["Close"]], ma_s, ma_l)
 
-        # verify last: price, signal, signal strength
+        # obtain last: price, signal, signal strength
         last_clo = df["Close"].iloc[-1]
         last_sig = df["Signal"].iloc[-1]
         last_str = df["Signal_Strength"].iloc[-1]
@@ -37,14 +38,13 @@ def main():
         # store report
         alerts.append({
             "Ticker": ticker,
-            "MA_S": pair[0],
-            "MA_L": pair[1],
+            "MA_S": ma_s,
+            "MA_L": ma_l,
             "Close": float(last_clo.iloc[0]),
             "Signal": int(last_sig),
             "Signal_Strength": int(last_str)
         })
-
-        report = [f"Report: {end}", f"SMA: {pair[0]}/{pair[1]}", ""]
+        report = [f"Report: {end}", f"SMA: {ma_s}/{ma_l}", ""]
     
     for a in alerts:
         s = a["Signal"]
@@ -56,13 +56,13 @@ def main():
             verb = "⬆️ BUY" if s == 1 else "⬇️ SELL"
             msg  = f"{a['Ticker']} | {verb} (SMA{a['MA_S']}/{a['MA_L']}) Strength {a['Signal_Strength']:d} | Price R${a['Close']:.2f}"
                 
-            # notifies via e-mail
+            # notifies via Telegram
             try:
                 tsf.send_telegram(msg)
             except Exception as err:
                 print("Telegram error:", err)
 
-    # export report
+    # export current report
     report_df = pd.DataFrame(report)
     report_df.to_excel("report/report.xlsx", index=False)
 
